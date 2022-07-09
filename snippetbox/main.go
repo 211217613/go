c2038946544dfe01aefdeef127c20c6f5b6d5b7c
211217613 <xxx@xxx.com>
2022-07-09 10:34:13 -0400

    update

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -7,25 +7,33 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	// Define a new command-line flag with the name 'addr'.
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
 	flag.Parse()
 
+	// setup loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
 	mux := http.NewServeMux()
 
+	// handle static assets
 	fileServer := http.FileServer(http.Dir("./ui/static"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	// Initialize a server struct and overwrite the ErrorLog field. This way the server will use the new custom error logger instead of the regular log.Error
 	srv := &http.Server{
